02f1cebeaed3bb89617bf80ac22c58ff16a5a933
Hari <119789941+hariprasanthhp@users.noreply.github.com>
2023-09-27 12:18:11 +0530

    ch

--- bamboo.sh
+++ bamboo.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 ng version
-node --max_old_space_size=6096 ./node_modules/@angular/cli/bin/ng build --configuration production
+node --max_old_space_size=8096 ./node_modules/@angular/cli/bin/ng build --configuration production
