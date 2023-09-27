490ff9f898f07ec5ee2e6aab1fcb0062a2d9dba2
Hari <119789941+hariprasanthhp@users.noreply.github.com>
2023-09-27 16:30:21 +0530

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
-docker rm -f occ-app-latest2-container
-docker rmi -f occ-app-latest-image2
 docker build -t occ-app-latest-image2 .
 docker run --name occ-app-latest2-container -d -p 9090:80 occ-app-latest-image2
